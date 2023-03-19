c68c01d1aa47e861b6698a3c5ebaaa5f06d9cece
Fabio-RibeiroB <fabio.brady@gmail.com>
2023-03-19 19:58:17 +0000

    No spam

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@
 
 import discord
 from discord.ext import tasks, commands
+import asyncio
 import subprocess
 import os
 import bot_config
@@ -33,21 +34,20 @@ async def send_discord_message(channel_id, message):
 async def check_container_status():
     container_status = get_container_status(CONTAINER_ID)
     if container_status == 'running':
-        message = f"The container {CONTAINER_ID} is now running."
+        message = f"Bedrock server is up."
         await send_discord_message(CHANNEL_ID, message)
     elif container_status == 'exited':
-        message = f"The container {CONTAINER_ID} has exited."
+        message = f"Bedrock server is down."
         await send_discord_message(CHANNEL_ID, message)
 
-@tasks.loop(seconds=1)
-async def check_login_status():
-    MC_LOG = f"/var/lib/docker/containers/{CONTAINER_ID}/{CONTAINER_ID}-json.log"
-    container = docker_client.containers.get(CONTAINER_ID)
-    log_cmd = f"tail -n 1 {MC_LOG} | awk -F'[:,]' '{{print $5,$6}}' | awk -F'[]]' '{{print $2}}'"
-    _, output = container.exec_run(log_cmd)
-    data = output.decode().strip()
-    if data:
-        await send_discord_message(CHANNEL_ID, data)
+#async def check_login_status():
+#    MC_LOG = f"/var/lib/docker/containers/{CONTAINER_ID}/{CONTAINER_ID}-json.log"
+#    container = docker_client.containers.get(CONTAINER_ID)
+#    log_cmd = f"tail -n 1 {MC_LOG} | awk -F'[:,]' '{{print $5,$6}}' | awk -F'[]]' '{{print $2}}'"
+#    _, output = container.exec_run(log_cmd)
+#    data = output.decode().strip()
+#    if data:
+#        await send_discord_message(CHANNEL_ID, data)
 
 #    subprocess.call("./login_check.sh")
 #    if os.stat("./login_check_result").st_size != 0:
@@ -56,16 +56,31 @@ async def check_login_status():
 @client.command()
 async def status(ctx):
     container_status = get_container_status(CONTAINER_ID)
-    message = f"The container {CONTAINER_ID} is currently {container_status}."
+    message = f"The Bedrock server is currently {container_status}."
     await ctx.send(message)
 
+@tasks.loop(seconds=5)
+async def loop():
+    subprocess.call("./login_check.sh")
+
+    if os.stat("./login_check_result").st_size != 0:
+        f = open("./login_check_result", 'r')
+        data = f.readline()
+        channel = client.get_channel(int(CHANNEL_ID))
+        await channel.send(data)
+        f.close()
 
 
 @client.event
 async def on_ready():
 	check_container_status.start()
-	check_login_status.start()
+	loop.start()
 	print('Logged in done. Bot is ready...')
 
 
-client.run(TOKEN)
+# client.run(TOKEN)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.start(TOKEN))
+    loop.run_forever()
